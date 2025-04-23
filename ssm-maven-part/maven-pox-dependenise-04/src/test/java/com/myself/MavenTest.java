cad9cbc641943b85049b7ad7ad532c40cdecad8c
MoonShlne <1747874921a@gmail.com>
2025-04-23 17:10:41 +0800

    maven finished learning

---
+++ ssm-maven-part/maven-pox-dependenise-04/src/test/java/com/myself/MavenTest.java
@@ -0,0 +1,16 @@
+package com.myself;
+
+
+import org.junit.Test;
+
+/**
+ * @author polar
+ * @version 1.0
+ * @since 2025/4/23 13:26
+ */
+public class MavenTest {
+    @Test
+    public void test(){
+        System.out.println(1);
+    }
+}
